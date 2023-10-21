561cc61e74dd0f8fb05b1c75c2e3dc1c43818d3c
VladSmetana <sme7anavlad@gmail.com>
2023-10-21 12:25:43 +0300

    completed second js homework

---
+++ task-4.js
@@ -0,0 +1,28 @@
+function getShippingCost(country){
+  const ticketToChina = 100;
+  const ticketToChile = 250;
+  const ticketToAustralia = 170;
+  const ticketToJamaica = 120;
+  switch(country){
+    case 'China':
+      return `Shipping to China will cost ${ticketToChina} credits`;
+      break;
+    case 'Chile':
+      return `Shipping to Chile will cost ${ticketToChile} credits`;
+      break;
+    case 'Australia':
+      return `Shipping to Australia will cost ${ticketToAustralia} credits`;
+      break;
+    case 'Jamaica':
+      return `Shipping to Jamaica will cost ${ticketToJamaica} credits`;
+      break;
+    default:
+      return`Sorry, there is no delivery to your country`;
+  }
+}
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
